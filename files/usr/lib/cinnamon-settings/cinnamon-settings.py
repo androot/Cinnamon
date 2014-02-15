187e567fde83ff9a7a9a7d38afde60fb7b82e609
Michael Webster <miketwebster@gmail.com>
2014-02-15 11:52:17 -0500

    Remove tacky fade-in stuff in cinnamon-settings

--- files/usr/lib/cinnamon-settings/cinnamon-settings.py
+++ files/usr/lib/cinnamon-settings/cinnamon-settings.py
@@ -95,10 +95,6 @@ def touch(fname, times=None):
     with file(fname, 'a'):
         os.utime(fname, times)
 
-class SurfaceWrapper:
-    def __init__(self, surface):
-        self.surface = surface
-
 class MainWindow:
 
     # Change pages
@@ -122,7 +118,6 @@ class MainWindow:
             self.button_back.show()
             self.current_sidepage = sidePage
             self.maybe_resize(sidePage)
-            GObject.timeout_add(250, self.fade_in)
         else:
             sidePage.build(self.advanced_mode)
 
@@ -163,6 +158,7 @@ class MainWindow:
         self.search_entry.connect("icon-press", self.onClearSearchBox)
         self.window.connect("destroy", self.quit)
         self.window.connect("key-press-event", self.on_keypress)
+        self.window.show()
 
         self.builder.connect_signals(self)
         self.window.set_has_resize_grip(False)
@@ -244,8 +240,7 @@ class MainWindow:
         self.window.connect("destroy", self.quit)
         self.button_cancel.connect("clicked", self.quit)
         self.button_back.connect('clicked', self.back_to_icon_view)
-        self.window.set_opacity(0)
-        self.window.show()
+
         self.calculate_bar_heights()
 
         # Select the first sidePage
@@ -254,7 +249,6 @@ class MainWindow:
             self.findPath(first_page_iter)
         else:
             self.search_entry.grab_focus()
-            self.fade_in()
 
     def on_keypress(self, widget, event):
         if event.keyval == Gdk.KEY_BackSpace and type(self.window.get_focus()) != Gtk.Entry and \
@@ -262,9 +256,6 @@ class MainWindow:
             self.back_to_icon_view(None)
             return True
         return False
-
-    def fade_in(self):
-        self.window.set_opacity(1.0)
 
     def force_advanced(self):
         ret = False
